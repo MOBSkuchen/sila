542051d52aa06c04baa76c688059d39dbfa5ffc7
MOBSkuchen <71943003+MOBSkuchen@users.noreply.github.com>
2025-01-25 23:45:50 +0100

    Better comp errors

--- src/comp_errors.rs
+++ src/comp_errors.rs
@@ -1,3 +1,6 @@
+use std::fmt;
+use crate::codeviz::print_code_error;
+use crate::filemanager::FileManager;
 use crate::lexer::CodePosition;
 
 #[derive(Debug)]
@@ -17,26 +20,40 @@ pub enum CodeErrorType {
 pub struct CodeError {
     pub position: CodePosition,
     pub code_error_type: CodeErrorType,
-    pub details: String
+    pub title: String,
+    pub footer: String,
+    pub pointer: Option<String>,
 }
 
 impl CodeError {
-    pub fn new(position: CodePosition, code_error_type: CodeErrorType, details: String) -> Self {
-        Self {position, code_error_type, details}
+    pub fn new(position: CodePosition, code_error_type: CodeErrorType, title: String, pointer: Option<String>, footer: String) -> Self {
+        Self {position, code_error_type, title, footer, pointer }
     }
-    
+
     pub fn new_unknown_char_error(position: CodePosition, c: char) -> Self {
-        Self::new(position, CodeErrorType::LexerUnexpectedChar, format!("Character '{}' is weird!", c))
+        Self::new(position, CodeErrorType::LexerUnknownChar, "Unknown character".to_string(), 
+                  Some("This one".to_string()), format!("Character '{}' is weird!", c))
     }
 
     pub fn new_unexpected_char_error(position: CodePosition, c: char) -> Self {
-        Self::new(position, CodeErrorType::LexerUnexpectedChar, format!("Character '{}' is invalid at this point!", c))
+        Self::new(position, CodeErrorType::LexerUnexpectedChar, "Unexpected character".to_string(),
+                  Some("This one".to_string()), format!("Character '{}' is invalid at this point!", c))
     }
 
     pub fn new_eof_error() -> Self {
-        Self::new(CodePosition::eof(), CodeErrorType::LexerUnexpectedChar, "Premature end of file!".to_string())
+        Self::new(CodePosition::eof(), CodeErrorType::LexerUnexpectedChar, "End of File".to_string(), None, "Premature end of file!".to_string())
+    }
+    
+    pub fn visualize_error(self, file_manager: &FileManager) {
+        print_code_error(self, file_manager)
     }
 }
 
 pub type CompResult<T> = Result<T, CompilerError>;
 pub type CodeResult<T> = Result<T, CodeError>;
+
+impl fmt::Display for CodeErrorType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
