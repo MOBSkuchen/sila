e903f25be30e66dba3165de478e2673662a15211
MOBSkuchen <71943003+MOBSkuchen@users.noreply.github.com>
2025-01-28 15:09:22 +0100

    Update clparser

--- src/clparser.rs
+++ src/clparser.rs
@@ -1,24 +1,21 @@
+use std::collections::HashMap;
 
-use crate::colorize_rs::AnsiColor;
 pub const NAME: &str = env!("CARGO_PKG_NAME");
 pub const VERSION: &str = env!("CARGO_PKG_VERSION");
 pub const DESCRIPTION: &str = env!("CARGO_PKG_DESCRIPTION");
-pub const AUTHOR: &str = env!("CARGO_PKG_AUTHORS");
 
-use colorize_rs::{color_enabled};
+use colorize_rs::{color_enabled, AnsiColor};
 use crate::clparser::ClParserError::{FlagMissingValue, TooFewArguments, TooManyArguments};
 
 #[derive(Debug)]
 pub enum ClParserError {
-    TooFewArguments(usize, usize),
-    TooManyArguments(usize),
+    TooFewArguments(usize),
+    TooManyArguments(String),
     FlagMissingValue(String)
 }
 
 pub type ClParserResult<T> = Result<T, ClParserError>;
 pub type ClParserResultCallQueue = ClParserResult<Vec<PendingCall>>;
-pub type ClParserResultCall = ClParserResult<Vec<String>>;
-pub type ClParserResultNone = ClParserResult<()>;
 pub type CallFunction = Box<dyn Fn(&ArgumentParser, &Vec<String>) -> bool>;
 
 #[macro_export] 
@@ -42,6 +39,13 @@ macro_rules! mk_clfn_static {
     };
 }
 
+#[macro_export]
+macro_rules! empty {
+    () => {
+        Box::new(|_, _| {false})
+    };
+}
+
 pub fn fetch_args_clean() -> Vec<String> {
     let args: Vec<String> = std::env::args().collect();
     args[1..].to_vec()
@@ -51,7 +55,7 @@ fn _print_help(argument_parser: &ArgumentParser) -> bool {
     let mut shorts: Vec<String> = argument_parser.positionals.iter().map(|x| {x.short()}).collect();
     shorts.append(&mut argument_parser.arguments.iter().map(|x| {x.short()}).collect());
     shorts.append(&mut argument_parser.flags.iter().map(|x| {x.short()}).collect());
-    println!("{} (v{}) usage => {}", argument_parser.prog, argument_parser.version, shorts.join(" "));
+    println!("{} ({}) usage => {}", argument_parser.prog.clone().bold().underlined().b_magenta(), ("v".to_string() + &*argument_parser.version.clone()).underlined().faint(), shorts.join(" "));
     for argument in &argument_parser.arguments {
         println!("-> {}", argument.get_description())
     }
@@ -64,15 +68,17 @@ fn _print_help(argument_parser: &ArgumentParser) -> bool {
         println!("-> {}", flag.get_description())
     }
     
-    false
+    true
 }
 
 fn _print_version(argument_parser: &ArgumentParser) -> bool {
-    println!("{} => Version {}", argument_parser.prog, argument_parser.version);
-    false
+    println!("{} ({}) : {}", argument_parser.prog.clone().bold().underlined().b_magenta(), 
+             ("v".to_string() + &*argument_parser.version.clone()).underlined().faint(), 
+             argument_parser.description.clone().bold().underlined().b_yellow());
+    true
 }
 
-fn _disable_color(argument_parser: &ArgumentParser) -> bool {
+fn _disable_color(_: &ArgumentParser) -> bool {
     color_enabled(false);
     false
 }
@@ -101,17 +107,17 @@ impl Argument {
 
     fn get_description(&self) -> String {
         if self.nargs < 2 {
-            format!("{} | {}", self.name, self.description)
+            format!("{} | {}", self.name.clone().bold().b_green(), self.description.clone().bold())
         } else {
-            format!("{} => {} | {}", self.name, self.args.join(" "), self.description)
+            format!("{} => {} | {}", self.name.clone().bold().b_green(), self.args.join(" ").yellow(), self.description.clone().bold())
         }
     }
 
     fn short(&self) -> String {
         if self.nargs < 2 {
-            format!("[{}]", self.name)
+            format!("[{}]", self.name.clone().bold().b_green())
         } else {
-            format!("[{} {}]", self.name, self.args.join(" "))
+            format!("[{} {}]", self.name.clone().bold().b_green(), self.args.join(" ").yellow())
         }
     }
 
@@ -126,34 +132,33 @@ pub struct Flag {
     value: bool,
     triggers: CallFunction,
     description: String,
-    allow_end: bool,
 }
 
 impl Flag {
-    pub fn new(name: String, mini: String, value: bool, triggers: CallFunction, description: String, allow_end: bool) -> Self {
+    pub fn new(name: String, mini: String, value: bool, triggers: CallFunction, description: String) -> Self {
         Self {
             name,
             mini,
             value,
             triggers,
-            description,
-            allow_end,
+            description
         }
     }
 
     fn get_description(&self) -> String {
         if self.value {
-            format!("{} / {} => <VALUE> | {}", self.name, self.mini, self.description)
+            format!("{} / {} => {} | {}", self.name.clone().bold().b_blue(), self.mini.clone().bold().b_cyan(),
+                    "...".to_string().yellow(), self.description.clone().bold())
         } else {
-            format!("{} / {} | {}", self.name, self.mini, self.description)
+            format!("{} / {} | {}", self.name.clone().bold().b_blue(), self.mini.clone().bold().b_cyan(), self.description.clone().bold())
         }
     }
 
     fn short(&self) -> String {
         if self.value {
-            format!("[{} / {} <VALUE>]", self.name, self.mini)
+            format!("[{} / {} {}]", self.name.clone().bold().b_blue(), self.mini.clone().bold().b_cyan(), "<VALUE>".to_string().bold().yellow())
         } else {
-            format!("[{} / {}]", self.name, self.mini)
+            format!("[{} / {}]", self.name.clone().bold().b_blue(), self.mini.clone().bold().b_cyan())
         }
     }
 
@@ -162,33 +167,41 @@ impl Flag {
     }
 }
 
-enum CallType {
+#[derive(Debug)]
+pub enum CallType {
     ARGUMENT,
     POSITIONAL,
     FLAG
 }
 
-struct PendingCall {
+#[derive(Debug)]
+pub struct PendingCall {
+    name: String,
     index: usize,
     args: Vec<String>,
     call_type: CallType
 }
 
 impl PendingCall {
-    pub fn new(index: usize, args: Vec<String>, call_type: CallType) -> Self {
-        Self { index, args, call_type}
+    pub fn new(name: String, index: usize, args: Vec<String>, call_type: CallType) -> Self {
+        Self { name, index, args, call_type}
     }
     
-    pub fn call(&self, argument_parser: &ArgumentParser) -> bool {
+    pub fn has_name(&self, name: String) -> bool {
+        self.name == name
+    }
+    
+    pub fn call(&self, argument_parser: &ArgumentParser, spec_args: Option<&Vec<String>>) -> bool {
+        let args = spec_args.or(Some(&self.args));
         match self.call_type {
             CallType::ARGUMENT => {
-                argument_parser.arguments[self.index].call(argument_parser, &self.args)
+                argument_parser.arguments[self.index].call(argument_parser, args.unwrap())
             }
             CallType::POSITIONAL => {
-                argument_parser.positionals[self.index].call(argument_parser, &self.args)
+                argument_parser.positionals[self.index].call(argument_parser, args.unwrap())
             }
             CallType::FLAG => {
-                argument_parser.flags[self.index].call(argument_parser, &self.args)
+                argument_parser.flags[self.index].call(argument_parser, args.unwrap())
             }
         }
     }
@@ -230,8 +243,8 @@ impl ArgumentParser {
     }
 
     fn parse_argument(&self, argument: &Argument, args: &Vec<String>) -> ClParserResult<Vec<String>> {
-        if args.len() < argument.nargs {
-            Err(TooFewArguments(argument.nargs, args.len()))
+        if args.len() < (argument.nargs+1) {
+            Err(TooFewArguments(argument.nargs))
         } else {
             Ok(args[1..argument.nargs+1].to_vec())
         }
@@ -241,7 +254,7 @@ impl ArgumentParser {
         let mut remove_list = vec![];
         let mut pending_calls = vec![];
         for (i, pos) in self.positionals.iter().enumerate() {
-            pending_calls.push(PendingCall::new(i, self.parse_argument(pos, args)?, CallType::POSITIONAL));
+            pending_calls.push(PendingCall::new(pos.name.clone(), i, self.parse_argument(pos, args)?, CallType::POSITIONAL));
             remove_list.append(&mut (i..pos.args.len()+1).collect());
         }
 
@@ -258,7 +271,7 @@ impl ArgumentParser {
         for (i, arg) in args.iter().enumerate() {
             for (n, darg) in (&self.arguments).iter().enumerate() {
                 if darg.name == *arg {
-                    pending_calls.push(PendingCall::new(n, self.parse_argument(darg, args)?, CallType::ARGUMENT));
+                    pending_calls.push(PendingCall::new(darg.name.clone(), n, self.parse_argument(darg, args)?, CallType::ARGUMENT));
                     remove_list.append(&mut (i..darg.args.len()+1).collect());
                 }
             }
@@ -289,15 +302,18 @@ impl ArgumentParser {
         Ok(val)
     }
 
-    fn parse_flags(&self, args: &mut Vec<String>) -> ClParserResultCallQueue {
+    fn parse_flags(&self, flag_map: &mut HashMap<String, Option<String>>, args: &mut Vec<String>) -> ClParserResultCallQueue {
         let mut remove_list = vec![];
         let mut pending_calls = vec![];
+        
         for i in 0..args.len() {
             let item = &args[i];
             for (n, flag) in (&self.flags).iter().enumerate() {
                 if item.starts_with(&flag.name) && (!flag.value || (&flag.name == item)) || 
                     item.starts_with(&flag.mini) && (!flag.value || (&flag.mini == item)) {
-                    pending_calls.push(PendingCall::new(n, self.parse_flag(flag, item, args, i)?, CallType::FLAG));
+                    let rgs = self.parse_flag(flag, item, args, i)?;
+                    flag_map.insert(flag.name.clone(), if flag.value {Some((&rgs[0]).clone())} else {None});
+                    pending_calls.push(PendingCall::new(flag.name.clone(), n, rgs, CallType::FLAG));
                     remove_list.push(i);
                     if flag.value { remove_list.push(i + 1); }
                 }
@@ -311,35 +327,43 @@ impl ArgumentParser {
         Ok(pending_calls)
     }
 
-    pub fn parse(&self, mut args: Vec<String>) -> ClParserResultNone {
+    pub fn parse(&self, mut args: Vec<String>, auto_help_nargs: bool) -> ClParserResult<(Vec<PendingCall>, HashMap<String, Option<String>>)> {
         let mut pending_calls = vec![];
-        pending_calls.append(&mut self.parse_flags(&mut args)?);
+        let mut flag_map = HashMap::new();
+
+        for flag in &self.flags {
+            flag_map.insert(flag.name.clone(), None);
+        }
+        
+        if auto_help_nargs && args.len() == 0 {
+            _print_help(&self);
+            return Ok((pending_calls, flag_map));
+        }
+        
+        pending_calls.append(&mut self.parse_flags(&mut flag_map, &mut args)?);
         pending_calls.append(&mut self.parse_arguments(&mut args)?);
         pending_calls.append(&mut self.parse_positionals(&mut args)?);
+        
         if args.len() > 0 {
-            Err(TooManyArguments(args.len()))
+            Err(TooManyArguments(args[0].clone()))
         } else {
-            for x in pending_calls {
-                if x.call(self) {break}
-            }
-            Ok(())
+            Ok((pending_calls, flag_map))
         }
     }
     
     pub fn handle_errors(&self, error: ClParserError) {
         match error {
-            TooFewArguments(expected, got) => {
-                println!("Invalid usage! Got too few arguments. Expected {}, but got {}", 
-                         expected.to_string().bold().underlined(), got.to_string().bold().underlined());
+            TooFewArguments(expected) => {
+                eprintln!("{}\n", format!("Invalid usage! Got too few arguments. Expected {} more", expected).b_red().bold());
                 _print_help(self);
             }
             TooManyArguments(got) => {
-                println!("Invalid usage! Got too many arguments. Got {} too many",
-                         got.to_string().bold().underlined());
+                let tp = if got.starts_with("-") {"Flag"} else {"Argument"};
+                eprintln!("{}\n", format!("Invalid usage! Unknown {} '{}'", tp, got).b_red().bold());
                 _print_help(self);
             }
             FlagMissingValue(flag) => {
-                println!("The flag '{}' is missing its value! Look at its description below:", flag);
+                eprintln!("{}\n", format!("The flag '{}' is missing its value! Look at its description below:", flag).b_red().bold());
                 _print_help(self);
             }
         }
@@ -351,9 +375,7 @@ impl ArgumentParser {
             "-h".to_string(),
             false,
             mk_clfn_ng!(_print_help),
-            "Get help".to_string(),
-            true
-        )
+            "Get help".to_string())
     }
 
     pub fn get_auto_version(&self) -> Flag {
@@ -361,10 +383,8 @@ impl ArgumentParser {
             "--version".to_string(),
             "-v".to_string(),
             false,
-            mk_clfn_ng!(_print_help),
-            "Get version".to_string(),
-            true
-        )
+            mk_clfn_ng!(_print_version),
+            "Get version".to_string())
     }
 
     pub fn get_auto_no_color(&self) -> Flag {
@@ -372,10 +392,8 @@ impl ArgumentParser {
             "--no-color".to_string(),
             "-nc".to_string(),
             false,
-            mk_clfn_ng!(_print_help),
-            "Disable colors".to_string(),
-            false
-        )
+            mk_clfn_ng!(_disable_color),
+            "Disable colors (please use as first argument / flag)".to_string())
     }
 
     pub fn add_help(&mut self) -> &mut Self {
