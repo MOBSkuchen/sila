19f9712f53dfc44e7dbeaba0ae25a290e657b696
MOBSkuchen <71943003+MOBSkuchen@users.noreply.github.com>
2025-02-03 11:33:55 +0100

    Extend parser to add function calls and returns

--- src/parser.rs
+++ src/parser.rs
@@ -33,18 +33,21 @@ impl Parser {
         false
     }
     
-    fn consume(&self, pointer: &mut usize, expected: TokenType) -> CodeResult<&Token> {
+    fn consume(&self, pointer: &mut usize, expected: TokenType, note: Option<String>) -> CodeResult<&Token> {
         if self.match_token(pointer, expected) {
             Ok(self.previous(pointer).unwrap())
         } else {
-            Err(CodeError::placeholder())
+            Err(CodeError::new_unexpected_token_error(self.current(pointer).unwrap(), expected, note))
         }
     }
     
     fn previous(&self, pointer: &usize) -> Option<&Token> {
-        self.tokens.get(*pointer)
+        self.tokens.get(*pointer-1)
     }
 
+    fn current(&self, pointer: &usize) -> Option<&Token> {
+        self.tokens.get(*pointer)
+    }
 
     pub fn parse(&self, pointer: &mut usize) -> CodeResult<Vec<ASTNode>> {
         let mut statements = Vec::new();
@@ -53,6 +56,7 @@ impl Parser {
             match token.token_type {
                 // Parse function definitions
                 TokenType::Define => {
+                    self.advance(pointer);
                     let func = self.parse_function(pointer)?;
                     statements.push(func);
                 }
@@ -75,27 +79,29 @@ impl Parser {
     // Parse import statement (assuming a simple import structure)
     fn parse_import(&self, pointer: &mut usize) -> CodeResult<ASTNode> {
         // Consume 'import' keyword
-        self.consume(pointer, TokenType::Import)?;
+        self.consume(pointer, TokenType::Import, None)?;
 
         // Expect an identifier for the import (e.g., module name)
-        let module_name = self.consume(pointer, TokenType::Identifier)?;
+        let module_name = self.consume(pointer, TokenType::Identifier, None)?;
 
         // Optionally, handle import paths or other structures here if needed
         Ok(ASTNode::Import(module_name))
     }
 
     pub fn parse_function(&self, pointer: &mut usize) -> CodeResult<ASTNode> {
-        let name = self.consume(pointer, TokenType::Identifier)?;
+        let name = self.consume(pointer, TokenType::Identifier, None)?;
 
-        self.consume(pointer, TokenType::LParen)?;
+        assert_eq!(self.previous(pointer).unwrap().token_type, TokenType::Identifier);
+
+        self.consume(pointer, TokenType::LParen, None)?;
 
         let args = self.parse_arguments(pointer)?;
 
-        self.consume(pointer, TokenType::RParen)?;
+        self.consume(pointer, TokenType::RParen, None)?;
 
-        self.consume(pointer, TokenType::Colon)?;
+        self.consume(pointer, TokenType::Colon, None)?;
         let return_type = self.parse_type(pointer)?;
-
+        
         let body = self.parse_block(pointer)?;
 
         Ok(ASTNode::FunctionDef(
@@ -108,11 +114,10 @@ impl Parser {
     }
 
     fn parse_block(&self, pointer: &mut usize) -> CodeResult<Vec<Box<ASTNode>>> {
-        self.consume(pointer, TokenType::LBrace)?;
+        self.consume(pointer, TokenType::LBrace, None)?;
 
         let mut statements = Vec::new();
 
-        // Parse statements inside the block
         while let Some(token) = self.peek(pointer) {
             if token.token_type == TokenType::RBrace {
                 break;
@@ -126,23 +131,57 @@ impl Parser {
             }
         }
 
-        self.consume(pointer, TokenType::RBrace)?;
+        self.consume(pointer, TokenType::RBrace, Some("You may be missing a semi colon".to_string()))?;
 
         Ok(statements)
     }
+    
+    fn parse_function_call(&self, pointer: &mut usize) -> CodeResult<ASTNode>  {
+        let name = self.previous(pointer).unwrap();
+        let mut paras = vec![];
+        while let Some(tok) = self.peek(pointer) {
+            paras.push(Box::new(self.parse_expression(pointer)?));
+            if self.match_token(pointer, TokenType::RParen) {
+                break
+            }
+            self.consume(pointer, TokenType::Comma, Some("Add a comma".to_string()))?;
+        }
+        Ok(ASTNode::FunctionCall(name, paras))
+    }
+    
+    fn parse_return(&self, pointer: &mut usize) -> CodeResult<ASTNode> {
+        self.consume(pointer, TokenType::Return, None)?;
+        Ok(ASTNode::Return(Box::new(self.parse_expression(pointer)?)))
+    }
+    
     fn parse_statement(&self, pointer: &mut usize) -> CodeResult<ASTNode> {
         let token = self.peek(pointer);
 
         if let Some(token) = token {
             match token.token_type {
-                TokenType::Identifier | TokenType::NumberInt | TokenType::NumberFloat => {
+                TokenType::Identifier => {
+                    self.advance(pointer);
+                    if self.match_token(pointer, TokenType::LParen) {
+                        self.parse_function_call(pointer)
+                    } else {
+                        // TODO: No effect warning
+                        self.parse_expression(pointer)
+                    }
+                } 
+                TokenType::NumberInt | TokenType::NumberFloat => {
+                    // TODO: No effect warning
                     self.parse_expression(pointer)
                 }
-                _ => {
+                TokenType::Return => {
+                    self.parse_return(pointer)
+                }
+                o => {
+                    panic!("145 => {:?}", o);
                     Err(CodeError::placeholder())
                 }
             }
         } else {
+            panic!("150");
             Err(CodeError::placeholder())
         }
     }
@@ -155,8 +194,8 @@ impl Parser {
                 break;
             }
 
-            let name = self.consume(pointer, TokenType::Identifier)?;
-            self.consume(pointer, TokenType::Colon)?;
+            let name = self.consume(pointer, TokenType::Identifier, None)?;
+            self.consume(pointer, TokenType::Colon, None)?;
             let arg_type = self.parse_type(pointer)?;
 
             arguments.push((name, Box::new(arg_type)));
@@ -227,17 +266,19 @@ impl Parser {
                     return if self.match_token(pointer, TokenType::RParen) {
                         Ok(expr)
                     } else {
+                        println!("LParen");
                         Err(CodeError::placeholder())
                     }
                 }
                 _ => {}
             }
         }
-        Err(CodeError::placeholder())
+        Err(CodeError::new_unexpected_token_error(self.previous(pointer).unwrap(), TokenType::Expression,
+                                                  Some("You may add a literal (number), string, variable, or a term here".to_string())))
     }
     
     fn parse_type(&self, pointer: &mut usize) -> CodeResult<ASTNode> {
-        Ok(ASTNode::Type(self.consume(pointer, TokenType::Identifier)?))
+        Ok(ASTNode::Type(self.consume(pointer, TokenType::Identifier, None)?))
     }
 }
 
@@ -269,4 +310,8 @@ pub enum ASTNode<'a> {
     VariableSet(&'a Token, Box<ASTNode<'a>>, Option<Box<ASTNode<'a>>>),
     // Lib name
     Import(&'a Token),
+    // Name, Arguments (expr)
+    FunctionCall(&'a Token, Vec<Box<ASTNode<'a>>>),
+    // Expr
+    Return(Box<ASTNode<'a>>)
 }
