4e6fc74894532ee6106ef40a9dfdfc132e82a025
MOBSkuchen <71943003+MOBSkuchen@users.noreply.github.com>
2025-02-03 11:57:33 +0100

    Codeviz for offsets

--- src/codeviz.rs
+++ src/codeviz.rs
@@ -3,20 +3,26 @@ use crate::comp_errors::CodeError;
 use crate::filemanager::FileManager;
 
 pub fn print_code_error(code_error: CodeError, file_manager: &FileManager) {
-    let snip = file_manager.get_code_snippet(&code_error.position)
-        .annotation(
+    let (mut snip, offset) = file_manager.get_code_snippet(&code_error.position);
+    snip = snip.annotation(
             match code_error.pointer {
                 None => {
-                    Level::Error.span(code_error.position.range())
+                    Level::Error.span(code_error.position.range(offset))
                 }
                 Some(_) => {
-                    Level::Error.span(code_error.position.range()).label(code_error.pointer.unwrap().leak())
+                    Level::Error.span(code_error.position.range(offset)).label(code_error.pointer.unwrap().leak())
                 }
             }
     );
     
+    let mut footers = vec![Level::Error.title(code_error.footer.as_str())];
+
+    for note in &code_error.notes {
+        footers.push(Level::Note.title(note))
+    }
+    
     let id_fmt = format!("{:#04x}", code_error.code_error_type as usize);
-    let msg = Level::Error.title(code_error.title.as_str()).id(&*id_fmt).snippet(snip).footer(Level::Error.title(code_error.footer.as_str()));
+    let msg = Level::Error.title(code_error.title.as_str()).id(&*id_fmt).snippet(snip).footers(footers);
 
     let renderer = Renderer::styled();
     anstream::println!("{}", renderer.render(msg));
